2cf412ff146a20290f2cb74d96f85cb6400e692a
khushab <khushabalam7@gmail.com>
2025-07-19 16:25:47 +0530

    Footer image

--- src/components/marketing/footer.tsx
+++ src/components/marketing/footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
                         <div className="flex flex-col items-start justify-start md:max-w-[300px]">
                             <div className="flex items-center gap-2">
                                 <Image
-                                    src="/icons/agencygrow.png"
+                                    src="/icons/AgencyGrow.png"
                                     alt="AgencyGrow"
                                     width={32}
                                     height={32}
